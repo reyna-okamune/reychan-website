d32fb83a599f2a9640b18c3cded39674112e7785
reyna aguirrre-okamune <142048091+reyna-okamune@users.noreply.github.com>
2023-09-30 01:33:14 -0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,52 @@
-
 import React from "react";
+
 export default function Projects() {
-    return (
-        <div id="projects" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '75px' }}>
-            <h1
-              style={{
-                fontFamily: 'Protomo, sans-serif',
-                fontSize: '60px',
-                color: 'white',
-                textShadow:  `
+    const styles = {
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+        },
+        header: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingBottom: '75px',
+        },
+        h1: {
+            fontFamily: 'Protomo, sans-serif',
+            fontSize: '60px',
+            color: 'white',
+            textShadow: `
                 3px 3px 3px rgba(255,33,156,0.9), 
                 -3px -3px 3px rgba(255,33,156,0.9), 
                 3px -3px 3px rgba(255,33,156,0.9), 
                 -3px 3px 3px rgba(255,33,156,0.9)
-              `,
-                textAlign: 'center',
-                paddingLeft: '30px',
-                paddingRight: '30px'
-              }}
-            >
-              PROJECTS
-            </h1>
-          </div>
+            `,
+            textAlign: 'center',
+            paddingLeft: '30px',
+            paddingRight: '30px',
+        },
+        '@media (max-width: 768px)': {
+            container: {
+                padding: '5%',
+            },
+            imageFrame: {
+                width: '5%'
+            },
+            textInfo: {
+                fontSize: '10px'
+            }
+        }
+    };
+
+    return (
+        <div id="projects" style={styles.container}>
+            <div style={styles.header}>
+                <h1 style={styles.h1}>PROJECTS</h1>
+            </div>
         </div>
-        );
+    );
 }
