e3444f23dc9e0d67acab798e0af8656ec92d9da5
reyna aguirrre-okamune <142048091+reyna-okamune@users.noreply.github.com>
2023-09-30 01:32:59 -0700

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,131 +1,110 @@
-// src/components/Skills.jsx
-
 import React from "react";
 import flyingPowerpuff from '../images/move-powerpuff-me.jpg';
 
 export default function Skills() {
-    return (
-        <div id="skills" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        
-            <h1
-                style={{
-                    fontFamily: 'Protomo, sans-serif',
-                    fontSize: '45px',
-                    color: 'white',
-                    textShadow: `
-                    3px 3px 3px rgba(255,33,156,0.9), 
-                    -3px -3px 3px rgba(255,33,156,0.9), 
-                    3px -3px 3px rgba(255,33,156,0.9), 
-                    -3px 3px 3px rgba(255,33,156,0.9)
-                `,
-                    textAlign: 'center',
-                    marginBottom: '50px',
-                    marginRight: '20px'
-                }}
-            >
-                SKILLS
-            </h1>
-
-            <img src={flyingPowerpuff} alt="powerpuff girl" style={{ 
-            width: '100px', 
+    const styles = {
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+            width: '100%',
+            paddingLeft: '10%',
+            paddingRight: '10%',
+        },
+        header: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        h1: {
+            fontFamily: 'Protomo, sans-serif',
+            fontSize: '45px',
+            color: 'white',
+            textShadow: `
+                3px 3px 3px rgba(255,33,156,0.9), 
+                -3px -3px 3px rgba(255,33,156,0.9), 
+                3px -3px 3px rgba(255,33,156,0.9), 
+                -3px 3px 3px rgba(255,33,156,0.9)
+            `,
+            textAlign: 'center',
+            marginBottom: '50px',
+            marginRight: '20px',
+        },
+        image: {
+            width: '100px',
             height: 'auto',
-            animation: 'flying 1s infinite'
-          
-           }} />
-           </div>
-      
-
-            <section 
-            style={{ 
-              marginBottom: '30px',
-              fontFamily: 'dogica, sans-serif',
-              color: 'white',
-              fontSize: '13px',
-              paddingLeft: '15px',
-              paddingRight: '15px'
-            }}>
-              <h2 style = {{ 
-                textShadow:  `
+            animation: 'flying 1s infinite',
+        },
+        section: {
+            marginBottom: '30px',
+            fontFamily: 'dogica, sans-serif',
+            color: 'white',
+            fontSize: '13px',
+            paddingLeft: '15px',
+            paddingRight: '15px',
+        },
+        h2: {
+            textShadow: `
                 2px 2px 2px rgba(255,33,156,0.7), 
                 -2px -2px 2px rgba(255,33,156,0.7), 
                 2px -2px 2px rgba(255,33,156,0.7), 
                 -2px 2px 2px rgba(255,33,156,0.7)
-                `,  
-                paddingBottom: '20px'
-              }}>LANGUAGES: </h2>
-                <ul style = {{  
-                textShadow:  `
+            `,
+            paddingBottom: '20px',
+        },
+        ul: {
+            textShadow: `
                 2px 2px 2px rgba(0, 181, 195, 0.7), 
                 -2px -2px 2px rgba(0, 181, 195, 0.7), 
                 2px -2px 2px rgba(0, 181, 195, 0.7), 
                 -2px 2px 2px rgba(0, 181, 195, 0.7)
-               `,}}>
-                <li>python, C#, C++, javascript, kotlin</li>
+            `,
+        },
+        '@media (max-width: 768px)': {
+            container: {
+                padding: '5%',
+            },
+            imageFrame: {
+                width: '5%'
+            },
+            textInfo: {
+                fontSize: '10px'
+            }
+        }
+        
+        
+    };
+
+    return (
+        <div id="skills" style={styles.container}>
+            <div style={styles.header}>
+                <h1 style={styles.h1}>SKILLS</h1>
+                <img src={flyingPowerpuff} alt="powerpuff girl" style={styles.image} />
+            </div>
+
+            <section style={styles.section}>
+                <h2 style={styles.h2}>LANGUAGES:</h2>
+                <ul style={{ ...styles.ul, fontSize: '13px' }}> {/* Adjusted font size */}
+                    <li>python, C#, C++, javascript, kotlin</li>
                 </ul>
             </section>
 
-            <section 
-            style={{ 
-              marginBottom: '30px',
-              fontFamily: 'dogica, sans-serif',
-              color: 'white',
-              fontSize: '13px',
-              paddingLeft: '15px',
-              paddingRight: '15px'
-            }}>
-              <h2 style = {{ 
-                textShadow:  `
-                2px 2px 2px rgba(255,33,156,0.7), 
-                -2px -2px 2px rgba(255,33,156,0.7), 
-                2px -2px 2px rgba(255,33,156,0.7), 
-                -2px 2px 2px rgba(255,33,156,0.7)
-                `,  
-                paddingBottom: '20px'
-              }}>IDEs: </h2>
-                <ul style = {{  
-                textShadow:  `
-                2px 2px 2px rgba(0, 181, 195, 0.7), 
-                -2px -2px 2px rgba(0, 181, 195, 0.7), 
-                2px -2px 2px rgba(0, 181, 195, 0.7), 
-                -2px 2px 2px rgba(0, 181, 195, 0.7)
-               `,}}>
-                <li>visual studio code, pycharm,</li>
-                <li>jupyter notebook, replit, </li>
-                <li>ubuntu, deepnote, android studio </li>
-                
+            <section style={styles.section}>
+                <h2 style={styles.h2}>IDEs:</h2>
+                <ul style={{ ...styles.ul, fontSize: '13.6px' }}> {/* Adjusted font size */}
+                    <li>visual studio code, pycharm,</li>
+                    <li>jupyter notebook, replit,</li>
+                    <li>ubuntu, deepnote, android studio</li>
                 </ul>
             </section>
 
-            <section 
-            style={{ 
-              marginBottom: '30px',
-              fontFamily: 'dogica, sans-serif',
-              color: 'white',
-              fontSize: '13px',
-              paddingLeft: '15px',
-              paddingRight: '15px'
-            }}>
-              <h2 style = {{ 
-                textShadow:  `
-                2px 2px 2px rgba(255,33,156,0.7), 
-                -2px -2px 2px rgba(255,33,156,0.7), 
-                2px -2px 2px rgba(255,33,156,0.7), 
-                -2px 2px 2px rgba(255,33,156,0.7)
-                `,  
-                paddingBottom: '20px'
-              }}>CERTIFICATES: </h2>
-                <ul style = {{  
-                textShadow:  `
-                2px 2px 2px rgba(0, 181, 195, 0.7), 
-                -2px -2px 2px rgba(0, 181, 195, 0.7), 
-                2px -2px 2px rgba(0, 181, 195, 0.7), 
-                -2px 2px 2px rgba(0, 181, 195, 0.7)
-               `,
-               fontSize: '11px',
-               }}>
-                <li>machine learning foundations - eCornell </li>
-                <li>foundational c# - microsoft</li>
+            <section style={styles.section}>
+                <h2 style={styles.h2}>CERTIFICATES:</h2>
+                <ul style={{ ...styles.ul, fontSize: '11.1px' }}> {/* Adjusted font size */}
+                    <li>machine learning foundations - eCornell</li>
+                    <li>foundational c# - microsoft</li>
                 </ul>
             </section>
         </div>
