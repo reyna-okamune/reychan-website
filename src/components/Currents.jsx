5bb0a33c25a63d654ae6d656e6aaa1b928ad4eb2
reyna aguirrre-okamune <142048091+reyna-okamune@users.noreply.github.com>
2023-09-30 13:20:13 -0700

    Update Currents.jsx

--- src/components/Currents.jsx
+++ src/components/Currents.jsx
@@ -14,7 +14,7 @@ export default function Currents() {
     const mobileStyles = {
         h1: {
             fontFamily: 'Protomo, sans-serif',
-            fontSize: '30px',
+            fontSize: '27.5px',
             color: 'white',
             textShadow: `
                 3px 3px 3px rgba(255,33,156,0.9),
